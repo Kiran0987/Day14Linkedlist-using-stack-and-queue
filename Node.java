2ac62c5781f0a98d318c62bb64d961965192c832
kiran <sharmakiran237@gmail.com>
2022-06-07 11:26:53 +0530

    Added using peek

--- Node.java
+++ Node.java
@@ -4,8 +4,8 @@ public class Node <T>{
 	T value;
 	Node next;
 
-	Node(T data) {
-		this.value = data;
+	Node(T value) {
+		this.value = value;
 		this.next = null;
 	}
 }
--- StackQueue.java
+++ StackQueue.java
@@ -1,43 +1,66 @@
 package com.bl.stack;
 
 public class StackQueue {
-	 Node top;
+	Node top;
 
-     public <T> void push(T x) {
-         Node newNode = new Node(x);
-         if (isEmpty()) {
-             top = newNode;
-         } else {
-             newNode.value = x;
-             newNode.next = top;
-             top = newNode;
-         }
-     }
+	public <T> void push(int x) {
+		Node newNode = new Node(x);
+		if (isEmpty()) {
+			top = newNode;
+		} else {
+			newNode.value = x;
+			newNode.next = top;
+			top = newNode;
+		}
+	}
 
-     public boolean isEmpty() {
-         return top == null;
-     }
+	public boolean isEmpty() {
+		return top == null;
+	}
 
-     public void display() {
-         if (top == null) {
-             System.out.printf("Empty stack");
-         } else {
-             Node temp = top;
-             while (temp != null) {
-                 System.out.print(temp.value + " ");
-                 temp = temp.next;
-             }
-         }
-     }
+	public void display() {
+		if (top == null) {
+			System.out.printf("\nStack is Empty");
+		} else {
+			Node temp = top;
+			while (temp != null) {
+				System.out.print(temp.value + " ");
+				temp = temp.next;
+			}
+		}
+	}
 
-     public static void main(String[] args) {
-         StackQueue list = new StackQueue();
-         list.push(70);
-         list.push(30);
-         list.push(56);
-         System.out.println("The Stack elements are :");
-         list.display();
-     }
+	public void pop() {
+		if (isEmpty()) {
+			System.out.print("\n\n Stack is EMPTY ");
+			return;
+		}
+		for (int i = 0; i < 3; i++) {
+			top = top.next;
+		}
+	}
 
- }
-
+	public <T> Object peek() {
+		if (!isEmpty()) {
+			System.out.println("Top data is :" + top.value);
+			return top.value;
+		} else {
+			System.out.println("Stack is empty");
+			return - 1 ;
+		}
+	}
+	public static void main(String[] args) {
+		System.out.println("Welcome Stack and Queue program");
+		StackQueue list = new StackQueue();
+		list.push(70);
+		list.push(30);
+		list.push(56);
+		list.peek();
+		System.out.println("The Stack elements are ");
+		list.display();
+		list.pop();
+		System.out.println("\nAfter deletion ");
+		list.display();
+		System.out.printf("\nTop element is %d\n", list.peek());
+	}
+}
